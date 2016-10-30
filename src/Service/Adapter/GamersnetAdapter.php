d4b725f5a0f0c4a6cb14a788b0a841bbba0db42b
webstylecenter <petervandam24@hotmail.com>
2016-10-30 17:40:46 +0100

    Added support for Gamersnet

---
+++ src/Service/Adapter/GamersnetAdapter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Service\Adapter;
+
+use Entity\FeedItem;
+use Zend\Feed\Reader\Feed\FeedInterface;
+use Zend\Feed\Reader\ReaderImportInterface;
+
+class GamersnetAdapter implements FeedAdapterInterface
+{
+    const FEED_URL = 'http://www.gamersnet.nl/rssnews.xml';
+    const NAME = 'Gamersnet';
+
+    /**
+     * @var ReaderImportInterface $reader
+     */
+    protected $reader;
+
+    /**
+     * @param ReaderImportInterface $reader
+     */
+    public function __construct(ReaderImportInterface $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    /**
+     * @return FeedItem[]
+     */
+    public function read()
+    {
+        $feed = $this->reader->import(self::FEED_URL);
+
+        $map = [];
+        foreach ($feed as $entry) {
+            /** @var $entry FeedInterface */
+
+            $content = strip_tags($entry->getDescription());
+            $content = trim(str_replace('Read more...', '', $content));
+
+            $map[] = new FeedItem(
+                $entry->getId(),
+                $entry->getTitle(),
+                $content,
+                $entry->getLink()
+            );
+        }
+
+        return $map;
+    }
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
+

96db119b8c1194225894de36aa0a268a651a7037
webstylecenter <petervandam24@hotmail.com>
2016-10-30 15:54:46 +0100

    Added artiestennieuws parser

---
+++ src/Service/Adapter/ArtiestennieuwsAdapter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Service\Adapter;
+
+use Entity\FeedItem;
+use Zend\Feed\Reader\Feed\FeedInterface;
+use Zend\Feed\Reader\ReaderImportInterface;
+use Zend\Feed\Reader\Http\Client;
+
+
+class ArtiestennieuwsAdapter implements FeedAdapterInterface
+{
+    const FEED_URL = 'http://www.artiestennieuws.nl/feed/';
+    const NAME = 'Artiesten Nieuws';
+
+    /**
+     * @var ReaderImportInterface $reader
+     */
+    protected $reader;
+
+    /**
+     * @param ReaderImportInterface $reader
+     */
+    public function __construct(ReaderImportInterface $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    /**
+     * @return FeedItem[]
+     */
+    public function read()
+    {
+        // TODO: Use ClientInterface to parse feed the usual way
+        $feedSrc = file_get_contents(self::FEED_URL);
+        $feed = $this->reader->importString($feedSrc);
+
+        $map = [];
+        foreach ($feed as $entry) {
+            /** @var $entry FeedInterface */
+
+            $content = strip_tags($entry->getDescription());
+            $content = trim(str_replace('Read more...', '', $content));
+
+            $map[] = new FeedItem(
+                $entry->getId(),
+                $entry->getTitle(),
+                $content,
+                $entry->getLink()
+            );
+        }
+
+        return $map;
+    }
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
+

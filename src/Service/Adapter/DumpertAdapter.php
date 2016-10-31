77b1e149ccf5d7bc4654c87a3c5c63a4b2458fcc
webstylecenter <petervandam24@hotmail.com>
2016-10-31 17:42:12 +0100

    Added support for Dumpert

---
+++ src/Service/Adapter/DumpertAdapter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Service\Adapter;
+
+use Entity\FeedItem;
+use Zend\Feed\Reader\Feed\FeedInterface;
+use Zend\Feed\Reader\ReaderImportInterface;
+
+class DumpertAdapter implements FeedAdapterInterface
+{
+    const FEED_URL = 'http://www.dumpert.nl/rss.xml.php ';
+    const NAME = 'Dumpert';
+
+    /**
+     * @var ReaderImportInterface $reader
+     */
+    protected $reader;
+
+    /**
+     * @param ReaderImportInterface $reader
+     */
+    public function __construct(ReaderImportInterface $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    /**
+     * @return FeedItem[]
+     */
+    public function read()
+    {
+        $feed = $this->reader->import(self::FEED_URL);
+
+        $map = [];
+        foreach ($feed as $entry) {
+            /** @var $entry FeedInterface */
+
+            $content = strip_tags($entry->getDescription());
+            $content = trim(str_replace('Read more...', '', $content));
+
+            $map[] = new FeedItem(
+                $entry->getId(),
+                $entry->getTitle(),
+                $content,
+                $entry->getLink(),
+                self::NAME
+            );
+        }
+
+        return $map;
+    }
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
+

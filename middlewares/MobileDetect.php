89a1555caaedd55e9f989ff42b5a8c9aa2931e9c
webstylecenter <petervandam24@hotmail.com>
2016-11-08 22:54:47 +0100

    Added iphone icon

--- middlewares/MobileDetect.php
+++ middlewares/MobileDetect.php
@@ -5,7 +5,7 @@ use \Silex\Application;
 $app->before(function() use ($app) {
 
     $detect = new Mobile_Detect();
-    if ($detect->isMobile() && $_SERVER['REQUEST_URI'] == '/') {
+    if ($detect->isMobile() && $_SERVER['REQUEST_URI'] === '/') {
         header('location: /m/');
         exit;
     }

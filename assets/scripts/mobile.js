6ac3b548c7bb86ff5b4295e2feb5de8d9947b8eb
webstylecenter <petervandam24@hotmail.com>
2016-11-11 20:58:42 +0100

    Added hold to pin

--- assets/scripts/mobile.js
+++ assets/scripts/mobile.js
@@ -29,5 +29,43 @@ $( document ).ready(function() {
                     alert('Item (un-)pinned!');
                 }
             });
+    });
+
+    function startTrigger(e,data) {
+        var $elem = $(this);
+        $elem.data('mouseheld_timeout', setTimeout(function() {
+            $elem.trigger('mouseheld');
+        }, e.data));
+    }
+
+    function stopTrigger() {
+        var $elem = $(this);
+        clearTimeout($elem.data('mouseheld_timeout'));
+    }
+
+
+    var mouseheld = $.event.special.mouseheld = {
+        setup: function(data) {
+            var $this = $(this);
+            $this.bind('mousedown', +data || mouseheld.time, startTrigger);
+            $this.bind('mouseleave mouseup', stopTrigger);
+        },
+        teardown: function() {
+            var $this = $(this);
+            $this.unbind('mousedown', startTrigger);
+            $this.unbind('mouseleave mouseup', stopTrigger);
+        },
+        time: 750 // default to 750ms
+    };
+
+    $(".listItem").bind('mouseheld', function(e) {
+        console.log('Held', e);
+        var pin = $(this).find('.pin');
+        $.ajax("/pin/" +$(pin).data('pin-id'))
+            .done(function(response) {
+                if (response == '1') {
+                    $(pin).parent().toggleClass('pinned');
+                }
+            });
     })
 });

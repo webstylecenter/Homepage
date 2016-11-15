3dee117de8832f24152a72ab0de794e07da8e193
webstylecenter <petervandam24@hotmail.com>
2016-11-16 00:06:04 +0100

    Small code improvements

--- controllers/FeedController.php
+++ controllers/FeedController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Entity\FeedItem;
+
 $app->match('/update/', function() use($app) {
     /** @var \Service\FeedService $feedService */
     $feedService = $app['feedService'];
@@ -15,7 +17,21 @@ $app->post('/add-item/', function() use($app) {
     /** @var \Service\FeedService $feedService */
     $feedService = $app['feedService'];
 
-    if ($feedService->addItem($_POST)) {
+    if (empty($_POST['title']) || empty($_POST['title']) || empty($_POST['url'])) {
+        return 'Error: No feed title and/or url entered!';
+    }
+
+    $currentTime = (new \DateTime())->format('Y-m-d H:i:s');
+    $feedItem = new FeedItem(
+        md5($currentTime),
+        $_POST['title'],
+        $_POST['description'],
+        strpos($_POST['url'], 'http') === 0 ? $_POST['url'] : 'http://' . $_POST['url'],
+        'userInput'
+    );
+
+    $feedItem->setPinned(true);
+    if ($feedService->addItem($feedItem)) {
         return 'Done';
     } else {
         return $feedService->getLastError();
@@ -29,7 +45,7 @@ $app->match('/refresh/{date}', function($date) use($app) {
 
     return json_encode([
         'html' => $app['twig']->render('home/newsfeed.html.twig', [
-                    'feedItems'=> $feedService->getFeedItems(999, $date),
+                    'feedItems'=> $feedService->getFeedItems(999, new \DateTime($date)),
                 ]),
         'refreshDate' => (new \DateTime())->format('Y-m-d H:i:s'),
     ]);

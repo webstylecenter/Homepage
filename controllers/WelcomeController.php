cb821b1a31dece8eb3222364243494dadfc39eac
webstylecenter <petervandam24@hotmail.com>
2016-11-15 20:55:55 +0100

    Welcome added the same check

--- controllers/WelcomeController.php
+++ controllers/WelcomeController.php
@@ -7,5 +7,11 @@ $app->match('/welcome/', function() use($app) {
 
     return $app['twig']->render('welcome/index.html.twig', [
         'forecast' => $weatherService->getForecastList(),
+        'lastUpdate' => [
+            'css_main' => filemtime(__DIR__ . '/../assets/css/style.css'),
+            'css_mobile' => filemtime(__DIR__ . '/../assets/css/mobile.css'),
+            'js_main' => filemtime(__DIR__ . '/../assets/scripts/main.js'),
+            'js_mobile' => filemtime(__DIR__ . '/../assets/scripts/mobile.js'),
+        ],
     ]);
 });

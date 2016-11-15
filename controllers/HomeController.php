30bf898e8ab087d43ba71fb9e4319c625a5ca167
webstylecenter <petervandam24@hotmail.com>
2016-11-15 20:53:24 +0100

    Makes sure the most recent css/js will be loaded if they are updated

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,11 +3,16 @@
 $homeController = function() use($app) {
     /** @var \Service\FeedService $feedService */
     $feedService = $app['feedService'];
-
     $templateFolder = $_SERVER['REQUEST_URI'] === '/m/' ? 'mobile' : 'home';
     return $app['twig']->render($templateFolder . '/index.html.twig', [
         'feedItems'=> $feedService->getFeedItems(),
         'feedItemTotals'=> $feedService->getFeedItemTotals(),
+        'lastUpdate' => [
+            'css_main' => filemtime(__DIR__ . '/../assets/css/style.css'),
+            'css_mobile' => filemtime(__DIR__ . '/../assets/css/mobile.css'),
+            'js_main' => filemtime(__DIR__ . '/../assets/scripts/main.js'),
+            'js_mobile' => filemtime(__DIR__ . '/../assets/scripts/mobile.js'),
+        ],
     ]);
 };
 

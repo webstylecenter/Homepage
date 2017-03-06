b28813a1def2a0c6275ebbdd83b399324675ceb5
webstylecenter <petervandam24@hotmail.com>
2017-03-06 18:10:24 +0100

    Force droplistservice to parse json as array

--- src/Service/DroplistService.php
+++ src/Service/DroplistService.php
@@ -17,7 +17,7 @@ class DroplistService
      */
     public function __construct()
     {
-        $this->images = json_decode(file_get_contents('http://pvd.onl/api.php?hash=4deaf5a24b4ee6f6d0135f1bc2214bc3'), true);
+        $this->images = (array) json_decode(file_get_contents('http://pvd.onl/api.php?hash=4deaf5a24b4ee6f6d0135f1bc2214bc3'), true);
     }
 
     /**

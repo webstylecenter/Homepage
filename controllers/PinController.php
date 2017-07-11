7281b7f3a6d5b393a024f1b9550caa8af499e353
webstylecenter <petervandam24@hotmail.com>
2017-07-11 14:53:50 +0200

    Route fix for pin

--- controllers/PinController.php
+++ controllers/PinController.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->get('/pin/{id}', function($id) use($app) {
+$app->get('/feed/pin/{id}', function($id) use($app) {
     /** @var \Service\FeedService $feedService */
     $feedService = $app['feedService'];
     return $feedService->pinItem($id);

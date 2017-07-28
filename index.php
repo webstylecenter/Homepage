3c749d454405bb602b870a67f38482694ceeac74
webstylecenter <petervandam24@hotmail.com>
2017-07-28 20:07:47 +0200

    Added sentry

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+(new Raven_Client('https://7198498aafe4456eabd52f2568790ba0:463e1380422c4357a8ff3ef1628c319a@sentry.io/197110'))->install();
+
 $app = new Silex\Application();
 
 require_once __DIR__ . '/app/config.php';

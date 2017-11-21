5ed826d6c13064cf9fecd76024759f0ae14fb5a5
webstylecenter <petervandam24@hotmail.com>
2017-11-21 20:19:48 +0100

    Updated chrome extension support

--- controllers/ChromeImportController.php
+++ controllers/ChromeImportController.php
@@ -2,40 +2,41 @@
 
 use Entity\FeedItem;
 
-$app->get('/chrome/import/', function() use($app) {
+$app->post('/chrome/import/', function() use($app) {
+    if (!isset($_POST['url'])) {
+        return json_encode([
+            'status' => 'fail',
+            'message' => 'Missing parameter(s): url'
+        ]);
+    }
 
-    $url = urldecode($_GET['url']);
-
-    $meta = new \Service\MetaService();
-    $metaData = $meta->getByUrl($url);
+    $metaData = (new \Service\MetaService)->getByUrl($_POST['url']);
 
     $feedItem = new FeedItem(
         md5((new \DateTime())->format('Y-m-d H:i:s')),
         $metaData->getTitle(),
         $metaData->getMetaDescription() ?: '',
-        $url,
+        $metaData->getUrl(),
         0
     );
+    $feedItem->setPinned(true);
 
     /** @var \Service\FeedService $feedService */
     $feedService = $app['feedService'];
-    $feedItem->setPinned(true);
+
     if (!$feedService->addItem($feedItem)) {
-        return $feedService->getLastError();
+        return json_encode([
+            'status' => 'fail',
+            'message' => $feedService->getLastError()
+        ]);
     }
 
-
-    return $app['twig']->render('chrome/imported.html.twig', [
-        'import' => [
+    return json_encode([
+        'status' => 'success',
+        'data' => [
             'title' => $metaData->getTitle(),
             'description' => $metaData->getMetaDescription() ?: '',
-            'url' => $url,
-        ],
-        'bodyClass' => 'chromeImport',
-        'lastUpdate' => [
-            'css_main' => filemtime(__DIR__ . '/../dist/css/style.css'),
-            'js_main' => filemtime(__DIR__ . '/../dist/js/app.js'),
-        ],
+            'url' => $metaData->getUrl(),
+        ]
     ]);
-
 });

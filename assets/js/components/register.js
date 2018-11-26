2beaebc80e222303e5e31055cbc493ffa932137f
Peter van Dam <peter@petervdam.nl>
2018-11-26 20:47:57 +0100

    Added register requirement tags (Frontend)

--- assets/js/components/register.js
+++ assets/js/components/register.js
@@ -2,7 +2,7 @@ $(function () {
     $('.registerBox input[type="submit"]').on('click', function (e) {
         if (!$('.registerBox input[type="checkbox"]').is(':checked')) {
             e.preventDefault();
-            showDialog('Registration can\'t continue', 'You must agree if you want to use our services');
+            alert('Registration can\'t continue, you must agree if you want to use Feednews.');
         }
     });
 });

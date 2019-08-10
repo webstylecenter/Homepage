94f3f6c8474036273107aa49136bf4faca7eed69
Peter van Dam <peter@petervdam.nl>
2019-08-10 17:37:44 +0200

    Configured Webpack

--- webpack/plugins/mjml-plugin.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const MjmlStore = require('tde-webpack-mjml-plugin');
-
-module.exports = function (webpack, config) {
-  webpack.plugins.push(new MjmlStore(config.components.mjml.source, {
-    extension: config.components.mjml.extension,
-    outputPath: config.components.mjml.dist,
-  }));
-};
